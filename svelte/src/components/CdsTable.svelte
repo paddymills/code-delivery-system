e3f031430eebe36b3be2c90e066740d4f067ea18
Patrick Miller <paddymills@pm.me>
2022-03-31 20:19:57 -0400

    refactor tabbed view

---
+++ svelte/src/components/CdsTable.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { Table, Tooltip } from 'sveltestrap';
+
+    export let tableData: {
+        header: string[],
+        rows: any,
+    };
+</script>
+
+<Table hover>
+    <thead>
+        <tr>
+            {#each tableData.header as col}
+                <th>{col}</th>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        {#each tableData.rows as row}
+            {@const uuid = Math.random().toString(36).slice(-6)}
+            <tr id={`row-${uuid}`}>
+                {#each row as col}
+                    <td>{ col }</td>
+                {/each}
+            </tr>
+            <Tooltip target={`row-${uuid}`} placement="bottom">{uuid}</Tooltip>
+        {/each}
+    </tbody>
+</Table>
--- svelte/src/components/TabbedView.svelte
+++ svelte/src/components/TabbedView.svelte
@@ -8,68 +8,57 @@
     TODO: program link
 -->
 
-<script>
-    import { TabContent, TabPane, Table } from 'sveltestrap';
+<script lang="ts">
+    import { TabContent, TabPane } from 'sveltestrap';
+    import CdsTable from './CdsTable.svelte';
 
     export let data;
+
+    const main_header = [ "Material Master", "Description", "Grade", "Program", "Checked", "Printed" ];
+    const parts_header = [ "Grade", "Thickness", "Sheet", "Program", "Printed" ];
+
+    const tabsWithData = () => {
+        return ['webs', 'flanges', 'parts'].filter(t => data[t]);
+    }
+
+    function getHeaderAndRows(tabName: string) {
+        switch (tabName) {
+            case 'webs':
+            case 'flanges':
+                return {
+                    header: main_header,
+                    rows: data[tabName].map(row => [
+                        row.sheet ? row.sheet.matl : "",
+                        row.sheet ? row.sheet.matl_desc : "",
+                        row.sheet ? row.sheet.grade : "",
+                        row.program || "",
+                        row.checked || "",
+                        row.printed || "",
+                    ])
+                }
+        
+            case 'parts':
+                return {
+                    header: parts_header,
+                    rows: data[tabName].map(row => [
+                        row.sheet ? row.sheet.grade : "",
+                        row.sheet ? row.sheet.thk : "",
+                        row.sheet ? row.sheet.ame : "",
+                        row.program || "",
+                        row.printed || "",
+                    ])
+                }
+
+            default:
+                console.log("Unmatched tab name: " + tabName);
+        }
+    }
 </script>
 
 <TabContent>
-    {#each ['webs', 'flanges'] as wf}
-        {#if data[wf]}
-            <TabPane tabId={wf} tab={wf.replace(/\b\w/, (c) => c.toUpperCase())} active={wf === 'webs'}>
-                <Table hover>
-                    <thead>
-                        <tr>
-                            <th>Material Master</th>
-                            <th>Description</th>
-                            <th>Grade</th>
-                            <th>Program</th>
-                            <th>Checked</th>
-                            <th>Printed</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {#each data[wf] as row}
-                            <tr>
-                                <td>{ row.sheet ? row.sheet.matl : "" }</td>
-                                <td>{ row.sheet ? row.sheet.matl_desc : "" }</td>
-                                <td>{ row.sheet ? row.sheet.grade : "" }</td>
-                                <td>{ row.program || "" }</td>
-                                <td>{ row.checked || "" }</td>
-                                <td>{ row.printed || "" }</td>
-                            </tr>
-                        {/each}
-                    </tbody>
-                </Table>
-            </TabPane>
-        {/if}
-    {/each}
-
-    {#if data.parts}
-        <TabPane tabId="parts" tab="Parts" active={!data.webs}>
-            <Table hover>
-                <thead>
-                    <tr>
-                        <th>Grade</th>
-                        <th>Thickness</th>
-                        <th>Sheet Name</th>
-                        <th>Program</th>
-                        <th>Printed</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {#each data.parts as row}
-                        <tr>
-                            <td>{ row.sheet ? row.sheet.grade : "" }</td>
-                            <td>{ row.sheet ? row.sheet.thk : "" }</td>
-                            <td>{ row.sheet ? row.sheet.ame : "" }</td>
-                            <td>{ row.program || "" }</td>
-                            <td>{ row.printed || "" }</td>
-                        </tr>
-                    {/each}
-                </tbody>
-            </Table>
+    {#each tabsWithData() as tab}
+        <TabPane tabId={tab} tab={tab.replace(/\b\w/, (c) => c.toUpperCase())} active={tab === tabsWithData()[0]}>
+            <CdsTable tableData={getHeaderAndRows(tab)} />
         </TabPane>
-    {/if}
+    {/each}
 </TabContent>
